95dc0ed140fe0a79508b310ed2d2939530cd6187
Edwin Suarez <edwin.suarez@gsdplus.com>
2021-04-11 13:14:42 -0500

    Imagen de fondo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="notfound">
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+ 
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+*{
+  margin: 0%;
+  padding: 0%;
+  box-sizing: border-box;
 }
+
+.notfound{
+  background-image: url("assets/Space.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  /*background-color: aqua;*/
+}
+
 </style>
